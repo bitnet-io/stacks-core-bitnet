2eda3e26083bd1e42dcea57946092d94a4b2f110
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-06-15 04:33:23 -0700

    Update server.rs

--- stackslib/src/net/server.rs
+++ stackslib/src/net/server.rs
@@ -227,8 +227,8 @@ impl HttpPeer {
         let client_addr = match socket.peer_addr() {
             Ok(addr) => addr,
             Err(e) => {
-                warn!("Failed to get peer address of {:?}: {:?}", &socket, &e);
-                let _ = network_state.deregister(event_id, &socket);
+        //        warn!("Failed to get peer address of {:?}: {:?}", &socket, &e);
+          //      let _ = network_state.deregister(event_id, &socket);
                 return Err(net_error::SocketError);
             }
         };
